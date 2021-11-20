0cf48fa4199206d4ed9f4c65b7955cf83ddad8c3
Leandro <leandrodasilvalara@yahoo.com.br>
2021-11-20 15:22:16 -0300

    :tada: iniciando

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet , Text, View } from "react-native";
+import { Provider } from "react-redux";
+import HomeScrreen from "./screens/HomeScrreen";
+import { store } from "./store";
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <HomeScrreen />
+    </Provider>
   );
 }
 
---
+++ screens/HomeScrreen.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { SafeAreaView, StyleSheet, Text, View, Image } from 'react-native'
+import tw from 'tailwind-react-native-classnames';
+
+
+const HomeScrreen = () => {
+    return (
+        <SafeAreaView style={tw`bg-white h-full`}>
+            <View style={tw`p-5`}>
+                <Image
+                    style={{
+                        width: 100,
+                        height: 100,
+                        resizeMode: "contain",
+                    }}
+                    source={{
+                        uri: "https://links.papareact.com/gzs",
+                    }}
+                    />
+            </View>
+        </SafeAreaView>
+    )
+}
+
+export default HomeScrreen
+
+const styles = StyleSheet.create({
+    text: {
+        color: "blue",
+    },
+});
