541e0be81312f43e912a15b5fb0d6e6445f0283f
Leandro <leandrodasilvalara@yahoo.com.br>
2021-11-21 20:17:56 -0300

    :construction: implementing React Native Navigation

--- App.js
+++ App.js
@@ -2,15 +2,38 @@ import React from "react";
 import { StyleSheet , Text, View } from "react-native";
 import { Provider } from "react-redux";
 import HomeScrreen from "./screens/HomeScrreen";
+import MapScreen from "./screens/MapScreen";
 import { store } from "./store";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import 'react-native-gesture-handler'
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
 export default function App() {
+  const stack = createStackNavigator();
+
   return (
     <Provider store={store}>
-      <SafeAreaProvider>
-          <HomeScrreen />
-      </SafeAreaProvider>
+        <NavigationContainer> 
+        <SafeAreaProvider>
+            <stack.Navigator>
+              <stack.Screen 
+                name='HomeScreen'
+                component={HomeScrreen}
+                Options={{
+                  headerShown: false,
+                }}
+              />
+              <stack.Screen 
+                name='MapScreen'
+                component={MapScreen}
+                Options={{
+                  headerShown: false,
+                }}
+              />
+            </stack.Navigator>
+        </SafeAreaProvider>
+        </NavigationContainer>     
     </Provider>
   );
 }
--- components/NavOptions.js
+++ components/NavOptions.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { FlatList,Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import tw from "tailwind-react-native-classnames";
 import { Icon } from 'react-native-elements';
+import { useNavigation } from '@react-navigation/core';
 
 
 const data = [ 
@@ -20,13 +21,17 @@ const data = [
 ];
 
 const NavOptions = () => {
+    const navigation = useNavigation();
+
     return (
        <FlatList
             data={data}
             horizontal
             keyExtractor={(item) => item.id}
             renderItem={({ item }) => (
-                <TouchableOpacity style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>
+                <TouchableOpacity
+                onPress={() => navigation.navigate(item.screen)}
+                style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>
                      <View>
                          <Image
                             style={{ width: 120, height: 120, resizeMode: "contain"}}
